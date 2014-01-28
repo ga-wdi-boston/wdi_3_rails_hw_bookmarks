eb5fae1ae3db8b8c86ce73e6a98b66ef07dbe4ed
Mike Stone <mikestone14@gmail.com>
2014-01-28 18:39:34 -0500

    Update and delete actions created and implemented

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,6 @@
 class BookmarksController < ApplicationController
+	before_action :find_bookmark, only: [:edit, :update, :destroy]
+
 	def index
 		@bookmarks = Bookmark.all.order(:title)
 	end
@@ -17,7 +19,36 @@ class BookmarksController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		@bookmark.assign_attributes(bookmark_params)
+
+		if @bookmark.save
+			flash[:notice] = 'Bookmark updated!'
+			redirect_to root_url
+		else
+			flash.now[:error] = @bookmark.errors.full_messages.join(', ')
+			render :edit
+		end
+	end
+
+	def destroy
+		if @bookmark.destroy
+			flash[:notice] = 'Bookmark deleted'
+			redirect_to root_url
+		else
+			flash.now[:error] = @bookmark.errors.full_messages.join(', ')
+			redirect_to :back
+		end
+	end
+
 	private
+
+	def find_bookmark
+		@bookmark = Bookmark.find(params[:id])
+	end
 
 	def bookmark_params
 		params.require(:bookmark).permit(:title, :url, :comment, :favorite)
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,6 @@
 class Bookmark < ActiveRecord::Base
 	validates_presence_of :title, :url
+	 validates :url, :format => { :with => /\Ahttp:\/\//,
+    :message => "must start with http://" }
 end
+
