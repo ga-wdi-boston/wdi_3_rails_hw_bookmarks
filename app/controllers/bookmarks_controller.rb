67958809963958a87dd748663feb474c7998a2a5
Prescott Murphy <prurph@gmail.com>
2014-01-28 18:11:48 -0500

    Add create method to bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,4 +6,20 @@ class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      flash[:notice] = "Created #{@bookmark[:title]}"
+      redirect_to @bookmark
+    else
+      flash.now[:error] = @bookmark.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url, :comment, :favorite)
+  end
 end
