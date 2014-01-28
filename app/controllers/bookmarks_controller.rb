805a12027bb9bb540b4cb6c35f85a5d3440550e5
Mike Stone <mikestone14@gmail.com>
2014-01-28 18:04:10 -0500

    Bookmarks creation and listing working

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,25 @@
 class BookmarksController < ApplicationController
 	def index
-		@bookmarks = Bookmark.all
+		@bookmarks = Bookmark.all.order(:title)
+	end
+	def new
+		@bookmark = Bookmark.new
+	end
+
+	def create
+		@bookmark = Bookmark.new(bookmark_params)
+		if @bookmark.save
+			flash[:notice] = 'Bookmark saved'
+			redirect_to root_url
+		else
+			flash.now[:error] = @bookmark.errors.full_messages.join(', ')
+			render :new
+		end
+	end
+
+	private
+
+	def bookmark_params
+		params.require(:bookmark).permit(:title, :url, :comment, :favorite)
 	end
 end
