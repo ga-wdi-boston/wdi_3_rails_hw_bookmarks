a018e6ececafae7beab64278651005aadb844cde
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-28 18:48:40 -0500

    index is working

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+	def index
+		@bookmarks = Bookmark.all
+	end
+
+	def show
+		@bookmark = Bookmark.find(params[:id])
+	end
+
+	def create
+		@bookmark = Bookmark.new(bookmark_params)
+
+		if @bookmark.save #or: @blat = Blat.create(params[:blat])
+			flash[:notice] = 'Created a new bookmark!' #We use flash[] because we want it to show on the *next* request.
+			redirect_to @bookmark
+		else
+			#Use flash.now[] because we want it to show on *this* request.
+			flash.now[:error] = @bookmark.errors.full_messages.join(', ')
+			render :new
+		end
+	end
+
+	private
+
+	def bookmark_params
+		params.require(:bookmark).permit(:url, :title)
+	end
+end
---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ActiveRecord::Base
+
+	validates_presence_of :title, :url
+	validates_length_of :title, maximum: 100
+
+end
