ac28cc380821ab195b4d7a2262abe32f0ee8710f
Prescott Murphy <prurph@gmail.com>
2014-01-28 18:56:03 -0500

    Add edit and update actions to bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -22,6 +22,23 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
   end
 
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.assign_attributes(bookmark_params)
+
+    if @bookmark.save
+      flash[:notice] = "Updated bookmark"
+      redirect_to @bookmark
+    else
+      flash.now[:errors] = @bookmark.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
   private
   def bookmark_params
     params.require(:bookmark).permit(:title, :url, :comment, :favorite)
