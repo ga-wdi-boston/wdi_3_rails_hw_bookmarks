4b30cc73c88f1b7742c9466268c1a01549e312a6
Jordan Morano <jordan.morano@gmail.com>
2014-01-28 19:46:19 -0500

    validations added to model and works

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ActiveRecord::Base
 # Validations
-  # validates :title, presence: true, length: { maximum: 140 }
-  # validates :body, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :url, presence: true, format: { with: /\b(http:)|(https:).*\b/}
 end
