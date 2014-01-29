b257d08f19bb4fd665d34fce500352d5a94b1708
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-01-29 08:40:11 -0500

    created database, various necessary files

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,20 @@
+class BookmarksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
