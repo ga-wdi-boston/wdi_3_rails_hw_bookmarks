b1b5550d2c4b7684c598945f4143f053b8a956d3
Jordan Morano <jordan.morano@gmail.com>
2014-01-29 08:46:34 -0500

    update shortened url to full path and add get route for cut and paste access

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Bookmarker::Application.routes.draw do
   resources :bookmarks
   put '/bookmarks/ext/:id' => 'bookmarks#ext', as: 'bookmarks_ext'
+  get '/bookmarks/ext/:id' => 'bookmarks#ext'
   root to: 'bookmarks#index'
 end
