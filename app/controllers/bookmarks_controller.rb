b28dbb66d080a9e759742ce60d70ed7f3966c2b6
Jack Henley <jack.d.henley@gmail.com>
2014-01-28 21:45:18 -0500

    environment set up

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,22 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
---
+++ db/migrate/20140129023053_create_bookmarks.rb
@@ -0,0 +1,12 @@
+class CreateBookmarks < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.text :url
+      t.text :title
+      t.text :comment
+      t.integer :favorite
+
+      t.timestamps
+    end
+  end
+end
