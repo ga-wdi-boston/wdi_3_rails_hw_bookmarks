c5c2f2285c61dcf7be411e4dd757c3e2b72558b1
David Fisher <tibbon@gmail.com>
2014-01-29 09:53:23 -0500

    Added a seed file with 3 basic bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bookmark.create(url: "http://4chan.org",
+                title: "4Chan",
+                comment: "Worst place on earth. I love it",
+                favorite: true)
+
+Bookmark.create(url: "http://reddit.com",
+                title: "Reddit",
+                comment: "This is where my time goes",
+                favorite: false)
+
+Bookmark.create(url: "http://news.ycombinator.com",
+                title: "yCombinator",
+                comment: "I pretend spending time here is useful",
+                favorite: true)
