8be008940fdb57fe54419c7f38d24927ec642f52
George Straehle <gstraehle@gmail.com>
2014-05-07 22:40:34 -0400

    Added seed data, RESTful routes, bookmarks cont.
    website now loads from index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,50 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#Bookmark.delete_all
 
-#Bookmark.create(name: 'book'....
+Bookmark.delete_all
+
+Bookmark.create(
+  url: 'https://pine.fm/LearnToProgram/',
+  title: 'learning ruby',
+  comment: 'the best ruby write',
+  category: 'useful',
+  is_favorite: true
+)
+Bookmark.create(
+  url: 'http://finance.yahoo.com/q?s=oled',
+  title: 'financial information',
+  comment: 'check on universal display corporation',
+  category: 'serious',
+  is_favorite: false
+)
+Bookmark.create(
+  url: 'http://www.weather.com',
+  title: 'weather',
+  comment: 'generic weather site',
+  category: 'useful',
+  is_favorite: false
+)
+Bookmark.create(
+  url: 'http://www.buzzfeed.com/',
+  title: 'cure for boredom',
+  comment: 'a good way to waste the afternoon',
+  category: 'funny',
+  is_favorite: false
+)
+Bookmark.create(
+  url: 'http://www.google.com',
+  title: 'Google search',
+  comment: 'the best search engine',
+  category: 'useful',
+  is_favorite: true
+)
+Bookmark.create(
+  url: 'https://www.chase.com/',
+  title: 'Chase bank',
+  comment: 'a good bank',
+  category: 'serious',
+  is_favorite: true
+)
+
+
