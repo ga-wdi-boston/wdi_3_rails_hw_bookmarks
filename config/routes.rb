c53cd3fa5f50e8285741be3538277aa9f02da7ac
Lisa Plesko <plesko.l@gmail.com>
2014-05-08 18:03:35 -0400

    Users can link from URL titles into SHOW pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  get '/bookmarks' => 'bookmarks#index'
-  get '/bookmarks/:id' => 'bookmarks#show'
+  root 'bookmarks#index'
+
+  resources :bookmarks
+
+  # get '/bookmarks' => 'bookmarks#index'
+  # get '/bookmarks/:id' => 'bookmarks#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
