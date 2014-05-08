6d663a5e038b0c4d5f10a562aea8d8ac64e2fb1d
Mike Talley <miketalley@hotmail.com>
2014-05-08 18:29:36 -0400

    Got bookmarkr add and edit working
    * Modified controller with new, create, edit, update
    * Created partial for edit and new views
    * Modified index links for title to route to show

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,6 +9,29 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
   end
 
+  def new
+    @bookmark = Bookmark.new
+  end
 
+  def create
+    bookmark = Bookmark.create(bookmark_params)
+    redirect_to bookmark
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    bookmark = Bookmark.find(params[:id])
+    bookmark.update(bookmark_params)
+    redirect_to bookmark
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:url, :title, :category, :is_favorite)
+  end
 
 end
