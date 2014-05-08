44b909e44d9fe2b27306b08935ad8e7532572742
George Straehle <gstraehle@gmail.com>
2014-05-07 20:40:41 -0400

    Added schema, and backtested migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Bookmark.delete_all
+
+#Bookmark.create(name: 'book'....
