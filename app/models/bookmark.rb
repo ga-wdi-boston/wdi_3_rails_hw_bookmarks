48b3d63229e1d8a1b35e1bb0f375f45e0051864c
Lisa Plesko <plesko.l@gmail.com>
2014-05-08 19:24:36 -0400

    Titles can link outwards to the web page they are pointing to

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,10 @@
 class Bookmark < ActiveRecord::Base
+  CATEGORY = ['Funny','Useful','Serious']
+
+  validates :title, presence: true
+  # validates :url, format: { with: /\A(http|https)\Z/i,
+  #                 message: "Must be a valid URL, starting with http or https" }
+
 end
+
+
