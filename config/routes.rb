a82e9949e22bb19fa98f703e7d2d26be70f4caeb
Ethan Gould <ethan.g.907@gmail.com>
2014-05-09 01:06:28 -0400

    edit config file to match new actions, add to CSS

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/bookmarks/serious', to: 'bookmarks#serious'
   get '/bookmarks/funny', to: 'bookmarks#funny'
   get '/bookmarks/useful', to: 'bookmarks#useful'
-
+  get '/', to: 'bookmarks#index'
   resources :bookmarks
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bookmark.delete_all
+
+
+Bookmark.create(url: 'http://www.youtube.com', title: 'Youtube', category: 'Funny' , comment: 'Great for watching videos', is_favorite: true)
+Bookmark.create(url: 'http://www.github.com', title: 'GitHub', category: 'Useful' , comment: 'Version control for projects', is_favorite: true)
+Bookmark.create(url: 'http://www.nytimes.com', title: 'New York Times', category: 'Serious' , comment: 'Stay up to date on things', is_favorite: false)
+Bookmark.create(url: 'http://www.pandora.com', title: 'Pandora', category: 'Useful' , comment: 'Listen to favorite music station', is_favorite: true)
+
+
