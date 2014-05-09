2f83794f3cac4c0078441fbef8a557515a6e2406
Christine Lafontant <cklafontant@gmail.com>
2014-05-08 23:19:02 -0400

    Added show view, other changes below
    * updated index page
    * reset database due to glitch

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,6 +4,14 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.order(:title)
   end
 
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
 
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url, :comment, :category, :is_fav)
+  end
 
 end
