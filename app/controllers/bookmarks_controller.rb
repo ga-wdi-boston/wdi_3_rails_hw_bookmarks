c17b9abeddae24fbd1ac1b63c35afefda34e079a
Ethan Gould <ethan.g.907@gmail.com>
2014-05-08 22:56:19 -0400

    added CRUD controllers added basic CSS

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,44 @@
 class BookmarksController < ApplicationController
 
   def index
+    @bookmarks = Bookmark.all
+  end
 
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.create(bookmark_params)
+    redirect_to bookmarks_path
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def serious
+    @bookmarks = Bookmark.where(category: 'Serious').order(:title)
+    render :index
+  end
+
+  def funny
+    @bookmarks = Bookmark.where(category: 'Funny').order(:title)
+    render :index
+  end
+
+  def useful
+    @bookmarks = Bookmark.where(category: 'Useful').order(:title)
+    render :index
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url, :category, :comment, :is_favorite)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get '/bookmarks/serious', to: 'bookmarks#serious'
+  get '/bookmarks/funny', to: 'bookmarks#funny'
+  get '/bookmarks/useful', to: 'bookmarks#useful'
+
   resources :bookmarks
 
 end
