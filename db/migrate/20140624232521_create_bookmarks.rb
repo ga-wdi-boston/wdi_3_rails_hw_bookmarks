5f4e3f3155457b768127cd0d9e7fa3eb41cf6c05
Emma Gardner <emmagardner@outlook.com>
2014-06-24 19:29:32 -0400

    Add bookmarks table as the first migration and first step for the bookmarks project.

---
+++ db/migrate/20140624232521_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.text :url
+      t.text :title
+      t.text :comment
+      t.text :category
+      t.boolean :is_checked_favorite
+    end
+  end
+end
