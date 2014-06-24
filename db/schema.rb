3a11bcce4168747a33248df918441715aa74f619
csb324 <csb324@gmail.com>
2014-06-24 18:18:34 -0400

    Create category table in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624220720) do
+ActiveRecord::Schema.define(version: 20140624221642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20140624220720) do
     t.text    "comment"
     t.integer "category_id"
     t.boolean "is_favorite"
+  end
+
+  create_table "categories", force: true do |t|
+    t.text "name", null: false
   end
 
 end
