f30039e15169d84bb113215110f9d413469c2cd5
Jake Austin <jakeaust07@gmail.com>
2014-06-24 19:46:39 -0400

    create bookmarks migrate, bookmark model validates URL

---
+++ db/migrate/20140624230333_create_bookmark.rb
@@ -0,0 +1,11 @@
+class CreateBookmark < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.text :url, null: false
+      t.text :title, null: false
+      t.text :comment
+      t.text :category
+      t.boolean :favorite, default: false
+    end
+  end
+end
