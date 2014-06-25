eec7944af810f561130021cb0dfa59825542dbad
Max Kohl <maxkohl810@gmail.com>
2014-06-25 11:21:29 -0400

    run migration to initialize the bookmarks table

---
+++ db/migrate/20140625150510_create_bookmarks.rb
@@ -0,0 +1,15 @@
+class CreateBookmarks < ActiveRecord::Migration
+  def up
+    create_table :bookmarks do |t|
+      t.text :url, null: false
+      t.text :title, null: false
+      t.text :comment
+      t.text :category
+      t.boolean :is_favorite, default: false
+    end
+  end
+
+  def down
+    drop_table :bookmarks
+  end
+end
