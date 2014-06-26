d7463456e842839f09b3e460948e66bfe5e1728b
testkitschen <katryn.mcintosh@gmail.com>
2014-06-25 22:02:59 -0400

    Create bookmarks resource, define routes for all standard RESTful actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookmark/index'
+  resources :bookmarks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
