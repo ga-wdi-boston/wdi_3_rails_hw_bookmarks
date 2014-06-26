f53656ae005d13895dc44f059a390a90d95fc5a5
zhbanton <zhbanton@gmail.com>
2014-06-26 19:56:07 -0400

    New bookmarks can be created

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,4 +4,25 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.all
   end
 
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to(bookmarks_path)
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit([:url, :title, :comment, :category, :is_favorite])
+  end
+
 end
