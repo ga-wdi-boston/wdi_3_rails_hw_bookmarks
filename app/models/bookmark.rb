35e3b03683f894ec5319e19074c5b65afeecc10b
Devin Dreszer <devindreszer@gmail.com>
2014-06-26 19:50:02 -0400

    Bookmarks have appropriate validations and show errors to the user

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,7 @@
 class Bookmark < ActiveRecord::Base
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :url, format: { with: /\Ahttp.+\z/ }
+  validates :is_favorite, inclusion: { in: [true, false] }
+  validates :category, inclusion: { in: ["Serious", "Funny", "Useful"] }
 end
