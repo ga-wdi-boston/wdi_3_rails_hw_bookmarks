85d845be7b9e84492ba27209fd65040eeda7326c
Devin Dreszer <devindreszer@gmail.com>
2014-06-26 18:56:28 -0400

    User can submit new bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,4 +9,26 @@ class BookmarksController < ApplicationController
   def show
     @bookmark = Bookmark.find(params[:id])
   end
+
+  # create a new instant of bookmark class
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  # create bookmark with provided user input and save to db
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # return parameters that are allowed to be edited by user
+  def bookmark_params
+    params.require(:bookmark).permit([:title, :url, :category, :is_favorite, :comment])
+  end
 end
