2014be8c473cb68f19de993553f9644e5a200eb6
Max Kohl <maxkohl810@gmail.com>
2014-06-25 21:41:09 -0400

    create bookmarks controller and view, create welcome controller and view   * this also routes welcome to the root of the application (done in routes file)

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140625150510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: true do |t|
+    t.text    "url",                         null: false
+    t.text    "title",                       null: false
+    t.text    "comment"
+    t.text    "category"
+    t.boolean "is_favorite", default: false
+  end
 
 end
