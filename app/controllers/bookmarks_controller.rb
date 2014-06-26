eca56033d90e75bb68bc0e5208c971dab12b559e
csb324 <csb324@gmail.com>
2014-06-26 17:06:50 -0400

    Controller stuff and url validation
    * Bookmark url requires http://
    * BookmarkController methods created

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,4 +7,38 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
   end
 
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+
+    if @bookmark.update(bookmark_params)
+      redirect_to @bookmark
+    else
+      render :edit
+    end
+
+  end
+
+
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit([:title, :comment])
+
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,3 @@
 class Bookmark < ActiveRecord::Base
-
+  validates :url, presence: true, format: {with: /\Ahttp:\/\//}
 end
