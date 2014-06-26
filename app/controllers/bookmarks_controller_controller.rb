692ed8a0202e5c2dfed9ffe79997517a7b6d2cfa
Julie <juliesof@gmail.com>
2014-06-26 19:21:09 -0400

    Bookmark index and bookmark partial (first drafts) are now available in views

---
+++ app/controllers/bookmarks_controller_controller.rb
@@ -0,0 +1,69 @@
+class BookmarksControllerController < ApplicationController
+
+  # def index
+  #   @songs = Song.all
+  # end
+
+  # #GET /products/:id (the prod number)
+  # def show
+  #   @song = Song.find(params[:id])
+  # end
+  # # controller => songs, action => show, id => whatever we use when we call this action
+
+  # #GET /products/new
+  # def new
+  #   @song = Song.new
+  # end
+
+  # #POST of /songs
+  # def create
+  #   #constructs and instantiates a new song instance from the form fields, which live in the params hash
+  #   @song = Song.new(song_params)
+  #   if @song.save #can be local var bc we arent using it anywhere outside this method
+  #     redirect_to song_path #after it saves the new song, this redirects (301) to the overall product list
+  #   else
+  #     render :new #will go back to the form to try again
+  #   end
+  # end
+
+  # def edit
+  #   @song = Song.find(params[:id])
+  # end
+
+  # #Update an exxisting model, Product
+  # #ProductsController#update (method for the controller)
+  # def update
+  #   @song = Song.find(params[:id])
+
+  #   #Using strong params update this product Product#update- mode
+  #   if @song.update(song_params)
+  #     redirect_to @song, notice: "You have updated the #{@song.name}"
+  #   else
+  #     # No worky, try again, show me the form you.
+  #     render :edit
+  #   end
+  # end
+
+  #   def destroy
+  #     @song = Song.find(params[:id])
+  #     @song.destroy
+
+  #     #Show all songs
+  #     redirect_to songs_path, notice: "You have deleted the song"
+  #   end
+  # end
+
+  # private
+
+  # #define which params are allowed to be used to create a song
+  # #these are called strong params- they are a security thing
+  # def song_params
+  #   params.require(:song).permit([:name, :description, :price])
+  # end
+
+
+
+
+
+
+end
