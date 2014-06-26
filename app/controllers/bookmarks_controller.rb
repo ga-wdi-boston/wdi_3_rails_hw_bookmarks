80a767182c5b4b2ff69599972fa62a7306d206d7
Devin Dreszer <devindreszer@gmail.com>
2014-06-26 18:25:32 -0400

    User can view all attributes of a specific bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,12 @@
 class BookmarksController < ApplicationController
-  # Show all bookmarks in alphabetical order
+
+  # return all bookmarks in alphabetical order
   def index
     @bookmarks = Bookmark.order(:title)
+  end
+
+  # return a specific bookmark
+  def show
+    @bookmark = Bookmark.find(params[:id])
   end
 end
