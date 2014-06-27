81bc56d319f8baef5f4839beece89847159e8bba
krk324 <krk32488@gmail.com>
2014-06-26 20:08:22 -0400

    App can now validate new/edit bookmark.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -38,7 +38,6 @@ class BookmarksController < ApplicationController
     end
   end
 
-
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
--- app/models/Bookmark.rb
+++ app/models/Bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ActiveRecord::Base
+  validates :url, presence: true, uniqueness: true, format:{:with => /https?:\/\/[\S]+/}
+  # validates that the name is unique
+  validates :title, presence: true
+
 end
