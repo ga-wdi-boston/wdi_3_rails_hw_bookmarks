1e84d815829a65102d4a74509647c84d6c2b2df7
Emma Gardner <emmagardner@outlook.com>
2014-06-26 21:39:36 -0400

    added actions to bookmarks_controller.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,4 +4,49 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.all
   end
 
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+
+    if @bookmark.update(bookmark_params)
+      redirect_to @bookmark, notice: "Bookmark updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+
+    @bookmark.destroy
+    redirect_to bookmarks_path, notice: "Bookmark deleted!"
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit([:url, :title, :comment, :category, :is_checked_favorite])
+  end
+
 end
