2db17d635ecf8a944fdb4e104947e0eed23f29f3
Blair Caple <blair@kaaterskil.com>
2014-06-26 22:13:57 -0400

    run generate controller bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,53 @@
+class BookmarksController < ApplicationController
+
+  # GET /bookmarks/new
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  # POST /bookmarks
+  def create
+    bookmark = Bookmark.new(bookmark_params)
+    if bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
+  end
+
+  # GET /bookmarks/:id/edit
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  # PATCH /bookmarks/:id
+  def update
+    bookmark = Bookmark.find(params[:id])
+    if bookmark.update(bookmark_params)
+      redirect_to bookmarks_path
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /bookmarks/:id
+  def destroy
+    obj = Bookmark.find(params[:id])
+    if obj.destroy
+      redirect_to bookmarks_path
+    else
+      render :show
+    end
+  end
+
+  # GET /
+  # GET /bookmarks
+  def index
+    @bookmarks = Bookmark.all.order(:title)
+  end
+
+  # GET /bookmarks/:id
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+end
