e9f8de3a92b4d4212f01978df4c8fc7fad405420
Max Kohl <maxkohl810@gmail.com>
2014-06-26 21:23:51 -0400

    user can see all attributes of a bookmark on a new page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,4 +6,23 @@ class BookmarksController < ApplicationController
     @commented_bookmarks = @bookmarks.select { |bookmark| bookmark.has_comment? }
   end
 
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
 end
