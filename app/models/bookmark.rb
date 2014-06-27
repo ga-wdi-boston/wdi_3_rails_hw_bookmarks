5642f32d30ec7cafe4afb9b2d5df0104b0b13806
Devin Dreszer <devindreszer@gmail.com>
2014-06-26 22:02:05 -0400

    Provides dropdown options for category and requires a URL

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,8 @@
 class Bookmark < ActiveRecord::Base
+  CATEGORIES = ["Serious", "Funny", "Useful"]
   validates :title, presence: true
   validates :url, presence: true
   validates :url, format: { with: /\Ahttp.+\z/ }
   validates :is_favorite, inclusion: { in: [true, false] }
-  validates :category, inclusion: { in: ["Serious", "Funny", "Useful"] }
+  validates :category, inclusion: { in: CATEGORIES }
 end
