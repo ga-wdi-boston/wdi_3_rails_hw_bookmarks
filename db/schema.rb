7dd0db709f12038357d06e40aec7b637074bf2eb
Blair Caple <blair@kaaterskil.com>
2014-06-26 21:57:16 -0400

    Add indices to bookmarks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626230143) do
+ActiveRecord::Schema.define(version: 20140626231745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,5 +25,9 @@ ActiveRecord::Schema.define(version: 20140626230143) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "bookmarks", ["category"], name: "index_bookmarks_on_category", using: :btree
+  add_index "bookmarks", ["title"], name: "index_bookmarks_on_title", using: :btree
+  add_index "bookmarks", ["url"], name: "index_bookmarks_on_url", unique: true, using: :btree
 
 end
