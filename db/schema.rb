14229f575db61e7c1fec20066bcbf89b97772577
csb324 <csb324@gmail.com>
2014-06-26 20:23:31 -0400

    Add created_at to bookmarks, and show date created on single page

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626224717) do
+ActiveRecord::Schema.define(version: 20140627001805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: true do |t|
-    t.text    "url",         null: false
-    t.text    "title",       null: false
-    t.text    "comment"
-    t.boolean "is_favorite"
-    t.text    "category"
+    t.text     "url",         null: false
+    t.text     "title",       null: false
+    t.text     "comment"
+    t.boolean  "is_favorite"
+    t.text     "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
