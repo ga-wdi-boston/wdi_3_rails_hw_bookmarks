64fb831db399016f0798730f58fc78ffbd6edcbf
Max Kohl <maxkohl810@gmail.com>
2014-06-26 22:09:02 -0400

    validate the title, url, and is_favorite attributes of a new or modified bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ActiveRecord::Base
+
+  validates :title, length: { minimum: 5 }
+  validates :url, presence: true, format: { :with => /\A(http:\/\/)/ }
+  validates :is_favorite, inclusion: { in: [true, false] }
+
+
   def has_comment?
     self.comment?
   end
