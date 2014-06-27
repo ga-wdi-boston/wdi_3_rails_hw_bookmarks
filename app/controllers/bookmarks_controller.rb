5f02ec900efd40fccd02a7aab4109cc2a4453891
Max Kohl <maxkohl810@gmail.com>
2014-06-26 21:38:02 -0400

    user can add a new bookmark to the list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -11,18 +11,30 @@ class BookmarksController < ApplicationController
   end
 
   def new
+    @bookmark = Bookmark.new
+  end
 
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
   end
 
   def edit
 
   end
 
-  def create
+  def destroy
 
   end
 
-  def destroy
+  private
 
+  def bookmark_params
+    params.require(:bookmark).permit([:title, :url, :comment, :category, :is_favorite])
   end
 end
