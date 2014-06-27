1d950e2ea64bb3545c6f5e2da4455c3cb5796807
EricRho <eric.rho.87@gmail.com>
2014-06-27 14:57:39 -0400

    Complete seeds.rb file for bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Bookmark.delete_all
 
 puts "Creating 4 bookmarks"
-bookmark_count = 4
+bookmark_count = 10
 
 # bookmark_count.times do |i|
 #   Bookmark.create!(url: "Bookmark #{i}",
@@ -12,8 +12,8 @@ bookmark_count = 4
 #                    )
 # end
 
-  Bookmark.create!(url: 'http://www.reddit.com', title: 'Reddit', comment: '', category: 'news', is_favourite: true)
-  Bookmark.create!(url: 'http://www.youtube.com', title: 'YouTube', comment: '', category: 'media', is_favourite: true)
-  Bookmark.create!(url: 'http://www.screenrant.com', title: 'Screenrant', comment: '', category: 'news', is_favourite: false)
-  Bookmark.create!(url: 'http://www.facebook.com', title: 'Facebook', comment: '', category: 'social', is_favourite: true)
-  Bookmark.create!(url: 'http://www.hotchinesebabes.com', title: 'Hot Chinese Babes', comment: '', category: 'fun', is_favourite: false)
+  Bookmark.create!(url: 'http://www.reddit.com', title: 'Reddit', comment: 'Front Page of the Internet', category: 'news', is_favourite: true)
+  Bookmark.create!(url: 'http://www.youtube.com', title: 'YouTube', comment: 'Videos', category: 'media', is_favourite: true)
+  Bookmark.create!(url: 'http://www.screenrant.com', title: 'Screenrant', comment: 'Movie News', category: 'news', is_favourite: false)
+  Bookmark.create!(url: 'http://www.facebook.com', title: 'Facebook', comment: 'Friendly Friends', category: 'social', is_favourite: true)
+  Bookmark.create!(url: 'http://www.hotchinesebabes.com', title: 'Hot Chinese Babes', comment: 'Not What You Expect', category: 'fun', is_favourite: false)
