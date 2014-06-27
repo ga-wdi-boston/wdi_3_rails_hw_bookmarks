9fd76123bc529afd21424889088b32ab8d565fde
Max Kohl <maxkohl810@gmail.com>
2014-06-26 20:59:55 -0400

    user can click on bookmark title and visit link in new tab

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Bookmark.delete_all
 
-Bookmark.create({url: 'www.espn.com', title: 'ESPN', comment: "ESPN's main website.", category: 'Serious', is_favorite: 'true'})
+Bookmark.create({url: 'http://www.espn.com', title: 'ESPN', comment: "ESPN's main website.", category: 'Serious', is_favorite: 'true'})
 
+Bookmark.create({url: 'http://www.facebook.com', title: 'Facebook', comment: "It's, well, Facebook.", category: 'Funny', is_favorite: 'true'})
 
-Bookmark.create({url: 'www.facebook.com', title: 'Facebook', comment: "It's, well, Facebook.", category: 'Funny', is_favorite: 'true'})
+Bookmark.create({url: 'http://www.deviantart.com', title: 'Deviantart', comment: "This site has some awesome art.", category: 'Useful', is_favorite: 'false'})
 
-Bookmark.create({url: 'www.deviantart.com', title: 'Deviantart', comment: "This site has some awesome art.", category: 'Useful', is_favorite: 'false'})
+Bookmark.create({url: 'http://www.ganggreennation.com', title: 'Gang Green Nation', comment: "The best New York Jets blog on the web.", category: 'Serious', is_favorite: false})
 
-Bookmark.create({url: 'www.ganggreennation.com', title: 'Gang Green Nation', comment: "The best New York Jets blog on the web.", category: 'Serious', is_favorite: false})
+Bookmark.create({url: 'http://www.reactiongifs.com', title: 'Reaction Gifs', category: 'Funny', is_favorite: false})
