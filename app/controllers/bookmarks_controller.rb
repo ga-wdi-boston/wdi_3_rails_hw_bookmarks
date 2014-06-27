1052432f4e9b2bf132e745a3ac89618f9bf69e49
Julie <juliesof@gmail.com>
2014-06-26 21:32:27 -0400

    Added and CRUD methods to the controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -11,61 +11,61 @@ class BookmarksController < ApplicationController
   end
 
   # #GET /products/:id (the prod number)
-  # def show
-  #   @song = Song.find(params[:id])
-  # end
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
   # # controller => songs, action => show, id => whatever we use when we call this action
 
   # #GET /products/new
-  # def new
-  #   @song = Song.new
-  # end
+  def new
+    @bookmark = Bookmark.new
+  end
 
   # #POST of /songs
-  # def create
-  #   #constructs and instantiates a new song instance from the form fields, which live in the params hash
-  #   @song = Song.new(song_params)
-  #   if @song.save #can be local var bc we arent using it anywhere outside this method
-  #     redirect_to song_path #after it saves the new song, this redirects (301) to the overall product list
-  #   else
-  #     render :new #will go back to the form to try again
-  #   end
-  # end
+  def create
+    #   #constructs and instantiates a new song instance from the form fields, which live in the params hash
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save #can be local var bc we arent using it anywhere outside this method
+      redirect_to bookmark_path #after it saves the new song, this redirects (301) to the overall product list
+    else
+      render :new #will go back to the form to try again
+    end
+  end
 
-  # def edit
-  #   @song = Song.find(params[:id])
-  # end
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
 
   # #Update an exxisting model, Product
   # #ProductsController#update (method for the controller)
-  # def update
-  #   @song = Song.find(params[:id])
+  def update
+    @bookmark = Bookmark.find(params[:id])
 
-  #   #Using strong params update this product Product#update- mode
-  #   if @song.update(song_params)
-  #     redirect_to @song, notice: "You have updated the #{@song.name}"
-  #   else
-  #     # No worky, try again, show me the form you.
-  #     render :edit
-  #   end
-  # end
+    #   #Using strong params update this product Product#update- mode
+    if @bookmark.update(bookmark_params)
+      redirect_to @bookmark, notice: "You have updated the #{@bookmark.name}"
+    else
+      # No worky, try again, show me the form you.
+      render :edit
+    end
+  end
 
-  #   def destroy
-  #     @song = Song.find(params[:id])
-  #     @song.destroy
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
 
-  #     #Show all songs
-  #     redirect_to songs_path, notice: "You have deleted the song"
-  #   end
-  # end
 
-  # private
+    redirect_to bookmarks_path, notice: "You have deleted the bookmark"
+  end
 
-  # #define which params are allowed to be used to create a song
-  # #these are called strong params- they are a security thing
-  # def bookmark_params
-  #   params.require(:bookmark).permit([:url, :title, :comment, :category, :checkbox])
-  # end
+
+private
+
+# #define which params are allowed to be used to create a song
+# #these are called strong params- they are a security thing
+def bookmark_params
+  params.require(:bookmark).permit([:url, :title, :comment, :category, :checkbox])
+end
 
 
 
