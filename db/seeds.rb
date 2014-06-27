7edb918cd1c15fa8e52dc35c6681592817b49fa6
dmcgann1 <dmcgann1@gmail.com>
2014-06-26 20:08:10 -0400

    index and show views working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bookmark.delete_all
+
+puts "Creating 5 Bookmarks"
+
+Bookmark.create!(url: 'http://www.apple.com', title: 'Apple', comment: 'Nice gadgets', category: 'technology', is_favorite: true)
+Bookmark.create!(url: 'http://www.google.com', title: 'Google', comment: 'Check out the self driving car', category: 'technology', is_favorite: true)
+Bookmark.create!(url: 'http://www.espn.go.com', title: 'ESPN', comment: 'Sports', category: 'Sports', is_favorite: false)
+Bookmark.create!(url: 'http://fortune.com', title: 'Fortune Magazine', category: 'business news', is_favorite: true)
+Bookmark.create!(url: 'http://www.cnbc.com', title: 'CNBC', comment: 'Check out stock prices', category: 'business news', is_favorite: false)
