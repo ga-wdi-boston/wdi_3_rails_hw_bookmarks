e20c2b5c237f0d8f5f336ab79b04a97974dc6f5e
brimatteng <btenggren20@gmail.com>
2014-06-26 20:38:42 -0400

    Set up Create and new with view and link in index for adding new bookmarks! Fun ! Happy!

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,4 +8,24 @@ class BookmarksController < ApplicationController
   def show
     @bookmark = Bookmark.find(params[:id])
   end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.create(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+    def bookmark_params
+      params.require(:bookmark).permit(:title, :url, :comment, :category, :favorite)
+    end
+
 end
