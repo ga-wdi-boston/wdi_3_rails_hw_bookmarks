5742659a828b0f36172c4601592c3b26c128ab62
Devin Dreszer <devindreszer@gmail.com>
2014-06-26 22:21:27 -0400

    Seed data has links to real urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,50 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Bookmark.delete_all
 
-puts "Creating 20 Bookmarks"
-bookmark_count = 20
-categories = ["Serious", "Funny", "Useful"]
-is_favorite_options = ["true", "false"]
+Bookmark.create!(
+  title: "Ruby on Rails Guides",
+  url: "http://guides.rubyonrails.org/",
+  comment: "Awesome guide!",
+  category: "Useful",
+  is_favorite: true
+  )
 
-bookmark_count.times do |i|
-  Bookmark.create!(
-    title: Faker::Company.name,
-    url: Faker::Internet.url,
-    comment: Faker::Lorem.sentence,
-    category: categories[rand(3)],
-    is_favorite: is_favorite_options[rand(2)]
-    )
-end
+Bookmark.create!(
+  title: "Rails Documentation",
+  url: "http://api.rubyonrails.org/",
+  comment: "Official Documentation",
+  category: "Useful",
+  is_favorite: false
+  )
+
+Bookmark.create!(
+  title: "CAP Theorem: Explained",
+  url: "http://robertgreiner.com/2014/06/cap-theorem-explained/",
+  comment: "",
+  category: "Serious",
+  is_favorite: false
+  )
+
+Bookmark.create!(
+  title: "An Introduction To Full-Stack JavaScript",
+  url: "http://www.smashingmagazine.com/2013/11/21/introduction-to-full-stack-javascript/",
+  comment: "Alternative to rails",
+  category: "Serious",
+  is_favorite: false
+  )
+
+Bookmark.create!(
+  title: "Angular, Ember, And Backbone: Which JavaScript Framework Is Right For You?",
+  url: "http://readwrite.com/2014/02/06/angular-backbone-ember-best-javascript-framework-for-you#awesm=~oIlDDgJzjmlbSG",
+  comment: "",
+  category: "Useful",
+  is_favorite: false
+  )
+
+Bookmark.create!(
+  title: "31 GIFs That Will Make You Laugh Every Time",
+  url: "http://www.buzzfeed.com/summeranne/gifs-that-will-make-you-laugh-every-time",
+  comment: "Awesome guide!",
+  category: "Funny",
+  is_favorite: true
+  )
