7fbc1b171db92f399bc59ba87e972612b38061b2
testkitschen <katryn.mcintosh@gmail.com>
2014-06-26 20:57:43 -0400

    Update seed so that fake urls are unique, add validation for unique url and require url starts with http

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,10 +2,9 @@ class Bookmark < ActiveRecord::Base
   #validate that we have a name
   validates :title, presence: true
 
-  #validates :url, uniqueness: true
+  validates :url, uniqueness: true
   validates :url, presence: true
 
 
-  #validate that the format is correct, dd.dd
-  #validates :url, format: { :with => /https?:\/\/[\S]+/}
+  validates :url, format: { :with => /https?:\/\/[\S]+/}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts "Creating 20 Bookmarks"
 bookmark_count = 20
 
 bookmark_count.times do |i|
-  Bookmark.create!(title: "Bookmark #{i}", url: "URL http://google")
+  Bookmark.create!(title: "Bookmark #{i}", url: "http://#{i}")
 end
 
 # This file should contain all the record creation needed to seed the database with its default values.
