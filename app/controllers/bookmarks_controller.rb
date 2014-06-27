fabef17037d7eeffe2f3be469aeb05ebd4c2de21
brimatteng <btenggren20@gmail.com>
2014-06-26 20:00:31 -0400

    Add link to from index to show for your viewing convenience, and Show has instance var.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,6 +2,10 @@ class BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
 
-    render text: "THIS WORK NOW"
+    #render text: "THIS WORK NOW"
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
   end
 end
