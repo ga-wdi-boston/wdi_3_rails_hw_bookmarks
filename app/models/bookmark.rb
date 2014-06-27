d00d2fee7e9a4a1b014b307a25bdab7b36d067aa
zhbanton <zhbanton@gmail.com>
2014-06-27 01:01:31 -0400

    Validates category

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,5 @@ class Bookmark < ActiveRecord::Base
   CATEGORIES = [['funny', 'funny'], ['serious', 'serious'], ['useful', 'useful']]
   validates :url, :format => URI::regexp(%w(http https))
   validates :title, presence: true
+  validates :category, inclusion: { in: CATEGORIES.flatten }
 end
