acd2a0bc54843af38cca96cc6f5421ab8e6dae6b
zhbanton <zhbanton@gmail.com>
2014-06-27 00:57:21 -0400

    Dropdown menus added and form styling improved

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ActiveRecord::Base
+  CATEGORIES = [['funny', 'funny'], ['serious', 'serious'], ['useful', 'useful']]
   validates :url, :format => URI::regexp(%w(http https))
   validates :title, presence: true
 end
