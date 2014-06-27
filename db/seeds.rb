c569631608356ab4963cdb491eb200f0324436a1
Jodi Detch <jodidetch@gmail.com>
2014-06-26 22:57:28 -0400

    App has faker gem & way to generate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Why do I need delete_all?
+Bookmark.delete_all
+
+ "All Bookmarks"
+
+ bookmark_count = 30
+
+ categories = ["Serious", "Funny", "Useful"]
+ is_favorite_options = ["true", "false"]
+
+ bookmark_count.times do |i|
+   Bookmark.create!(
+     title: Faker::Company.name,
+     url: Faker::Internet.url,
+     comment: Faker::Lorem.sentence,
+     category: categories[rand(3)],
+     is_favorite: is_favorite_options[rand(2)]
+     )
+ end
