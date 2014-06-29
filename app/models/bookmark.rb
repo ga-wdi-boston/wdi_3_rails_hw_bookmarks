717ade9ba9d71641ba1639c7301f96108162a891
Julie <juliesof@gmail.com>
2014-06-29 12:04:20 -0400

    Validations for http:// presence and title presence

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,22 +2,22 @@ class Bookmark < ActiveRecord::Base
 
   VALID_CATEGORIES = [:serious, :funny, :thoughtful]
 
-  scope :category, -> (category) { where category: category }
-  scope :location, -> (location_id) { where location_id: location_id }
-  scope :starts_with, -> (name) { where("name like ?", "#{name}%")}
+  # scope :category, -> (category) { where category: category }
+  # scope :location, -> (location_id) { where location_id: location_id }
+  # scope :starts_with, -> (name) { where("name like ?", "#{name}%")}
 
 
 
 
-  # validates :url, presence: true
-  # validates :url, uniqueness: true
-  # validates :url, :format => URI::regexp(%w(http https))
+  validates :url, presence: true
+  validates :url, uniqueness: true
+  validates :url, :format => URI::regexp(%w(http https))
 
 
-  # validates :title, presence: true
-  # validates :title, uniqueness: true
+  validates :title, presence: true
+  validates :title, uniqueness: true
 
-  # validates :category, presence: true
-  # validates :category, uniqueness: true
+  validates :category, presence: true
+  validates :category, uniqueness: true
 
 end
