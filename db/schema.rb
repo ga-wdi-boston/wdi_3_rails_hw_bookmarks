4c2fb3472a624b16fe67120ddd7718e22bead86e
testkitschen <katryn.mcintosh@gmail.com>
2014-06-29 11:46:14 -0400

    Change database column name in categories table from category to name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628202009) do
+ActiveRecord::Schema.define(version: 20140629153358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,11 +30,11 @@ ActiveRecord::Schema.define(version: 20140628202009) do
   add_index "bookmarks", ["title"], name: "index_bookmarks_on_title", using: :btree
 
   create_table "categories", force: true do |t|
-    t.text     "category"
+    t.text     "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "categories", ["category"], name: "index_categories_on_category", using: :btree
+  add_index "categories", ["name"], name: "index_categories_on_name", using: :btree
 
 end
