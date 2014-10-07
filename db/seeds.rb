aa21e7a7e5ea5d4e3eee58d5157a5344a6e5ceb8
raielin@gmail.com <raielin@gmail.com>
2014-10-07 19:13:52 -0400

    Initial file setup. Updated Gemfile to require local version of Ruby. Created FRAMEWORK.md outlining requirements for app creation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+bookmarks_list = [
+  ["Google", "Useful", "Search Engine", true, "http://www.google.com"],
+  ["New York Times", "Serious", "News", true, "http://www.nytimes.com"],
+  ["Just Jared", "Funny", "Celebrity Nonsense", false, "http://www.justjared.com"],
+  ["Pinterest", "Useful", "Ideas, photos, other cool stuff", false, "http://www.pinterest.com"],
+  ["General Assembly", "Serious", "All things WDI", true, "http://generalassemb.ly"],
+  ["Amazon", "Useful", "Shopping mecca", true, "http://www.amazon.com"],
+  ["CelticsBlog", "Useful", "Celtics news, scores, discussion, and other stuff.", true, "http://www.celticsblog.com"],
+  ["The Ultimate Siri Guide", "Useful", "Walkthrough of most important Siri dictation commands.", false, "http://www.idownloadblog.com/2014/03/08/siri-guide/"]
+]
+
+bookmarks_list.each do |title, category, description, favorite, url|
+  Bookmark.create( title: title, category: category, description: description, favorite: favorite, url: url)
+end
