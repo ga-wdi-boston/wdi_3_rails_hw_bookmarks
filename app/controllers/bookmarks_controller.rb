6cbfbaf637f18e3138dc7b203f8dffc01048da72
George <gbanis@gmail.com>
2014-10-07 17:59:32 -0400

    Created index

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,9 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only [:show, :edit, :update, :destroy]
+  before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @bookmarks = Bookmark.all
+  end
 
   private
   def set_bookmark
