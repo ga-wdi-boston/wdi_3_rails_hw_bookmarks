723fe3479a3d4744533dceae5baccf5c2038113b
shaai <shaai.sattar@gmail.com>
2014-10-07 23:33:45 -0400

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'bookmarks' => 'bookmarks#index'
+  get 'new' => 'bookmarks#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
