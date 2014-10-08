3e09124f6ee0cbe992d8afc915e303038860d438
fishermanswharff <fishermanswharff@github.com>
2014-10-08 13:57:01 -0400

    touching up appliation herb file, seeds.rb for db population upon install.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+Bookmark.create(title: "Google Search", url: 'http://www.google.com', category: 'Search',checkbox: true)
+Bookmark.create(title: 'JW dot com', url: 'http://www.jasonwharff.com', category: 'Portfolio')
+Bookmark.create(title: 'Rails API', url: 'http://api.rubyonrails.org', category: 'Web Development', checkbox: true)
