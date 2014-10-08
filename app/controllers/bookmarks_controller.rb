93c6f69f9a6cbb70e5fde10145f78510044eceb7
MDROR <dymmchl@yahoo.com>
2014-10-07 20:12:15 -0400

    change ruby v in gem file, config routes added model and migrated

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,13 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def set_bookmark
+  end
+
+  def show
+  end
+end
---
+++ db/migrate/20141007211437_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.string :url, null: false
+      t.string :title, null: false
+      t.text :description
+      t.string :category
+      t.boolean :favorite
+    end
+  end
+end
