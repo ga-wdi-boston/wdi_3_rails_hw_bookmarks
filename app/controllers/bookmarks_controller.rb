7f25f95c3f83819567518ae61c18ffa8a21f6b69
Billy1339 <william.m.sargent@gmail.com>
2014-10-07 22:22:06 -0400

    lots of comments in the files

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,24 +3,31 @@ class BookmarksController < ApplicationController
   #im not sure if i really nead these or what they truly do,
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
 
+  #method index collects all the instances of bookmarks
   def index
     @bookmarks = Bookmark.all
     # @title = Bookmark.title
 
   end
 
-
+# method show GETS the bookmark and shows it in a new page
+#based upon the id of the bookmark within the table
   def show
   end
 
+
+#this method allows the user to input the new bookmark
   def new
     @bookmark = Bookmark.new
   end
 
+#this method allows the user to actually edit the bookmark
+#with the user input boxes
   def edit
   end
 
-
+#this method is the method that actually creates the new bookmark
+#from the input...still kinda magic!
   def create
     @bookmark = Bookmark.new(bookmark_params)
 
@@ -31,6 +38,8 @@ class BookmarksController < ApplicationController
     end
   end
 
+#this method is the method that actually updates the new bookmark
+#from the input...still kinda magic!
   def update
     respond_to do |format|
       if @bookmark.update(bookmark_params)
@@ -41,6 +50,7 @@ class BookmarksController < ApplicationController
     end
   end
 
+#this method destroys the bookmark...still kinda magic!
   def destroy
     @bookmark.destroy
     respond_to do |format|
@@ -49,11 +59,11 @@ class BookmarksController < ApplicationController
   end
 
   private
-
+#magic? not sure if necessary
   def set_bookmark
     @bookmark = Bookmark.find(params[:id])
   end
-
+#gets the parameters from teh user
   def bookmark_params
     params.require(:bookmark).permit(:url, :title, :description, :category, :favorite)
   end
