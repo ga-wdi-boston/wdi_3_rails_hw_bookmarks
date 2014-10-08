eaca1746efb52bbd0b35fe72363f368b7354329d
fishermanswharff <fishermanswharff@github.com>
2014-10-08 01:24:05 -0400

    adding in delete capability for bookmarkr

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,6 @@
 class Bookmark < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :tags, dependent: :destroy
-  validates :url,:title, presence: true
+  validates :title, presence: true
+  validates :url, presence:true #, format: { with: //, message: 'Must be a proper URL!'}
 end
