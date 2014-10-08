b303d4e103cfa9e23f972674f9b7a87e5907c60e
shaynaCummings <shayna.cummings@gmail.com>
2014-10-08 11:23:17 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+  # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Bookmark.create(title: "New York Times", url: "http://nyt.com", description: "", category: :helpful, favorite_flag: false)
+
+Bookmark.create(title: "Gawker", url: "http://gawker.com", description: "indulgence", category: :funny, favorite_flag: true)
