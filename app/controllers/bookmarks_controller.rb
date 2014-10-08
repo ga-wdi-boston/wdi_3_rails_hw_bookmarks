9bc841486f35921f6fe68c9af15b8f600e392a2a
MDROR <dymmchl@yahoo.com>
2014-10-07 21:54:07 -0400

    added destroy and edit actions and views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,6 +12,29 @@ class BookmarksController < ApplicationController
   def show
   end
 
+  def create
+    @bookmark = Bookmark.create(bookmark_params)
+    if @bookmark.save
+      flash[:notice] = 'Bookmark created!'
+      redirect_to bookmarks_path
+    else
+      flash.now[:alert]=@bookmark.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def destroy
+    @bookmark = Bookmark.destroy(params[:id])
+  end
 
 
+private
+    def bookmark_params
+      params.require(:bookmark).permit(:url, :title, :description, :category, :favorite)
+    end
 end
+
