fe1b75ec23e0458eb86cad2a56150f61ca0f6c1b
raielin@gmail.com <raielin@gmail.com>
2014-10-08 19:42:34 -0400

    Updated Bookmarks controller to reflect corrections reviewed in class today.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,35 +1,20 @@
 class BookmarksController < ApplicationController
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
 
-  # GET /bookmarks
   def index
-    @bookmarks = Bookmark.all
+    @bookmarks = Bookmark.order(:title)
   end
 
-  # GET /bookmarks/1
-  # The `1` here indicates a specific entry of bookmark with ID 1: Retrieves a single bookmark.
   def show
-    # @bookmark = Bookmark.find params[:id]
-
-    # to show what's actually being passed into @bookmark, put binding.pry here.
-
-    # Same as above:
-      # bookmark_id = params[:id]
-      # @bookmark = Bookmark.find bookmark_id
-
-    # @bookmark alone would return an array of a single bookmark's elements.
   end
 
-  # GET /bookmarks/new
   def new
     @bookmark = Bookmark.new
   end
 
-  # GET /bookmarks/1/edit
   def edit
   end
 
-  # POST /bookmarks
   def create
     @bookmark = Bookmark.new(bookmark_params)
 
@@ -40,22 +25,17 @@ class BookmarksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /bookmarks/1
   def update
-
-    if @bookmark.update(bookmark_params)
+    if @bookmark.update_attributes(bookmark_params)
       redirect_to @bookmark, notice: 'Bookmark was successfully updated.'
     else
       render :edit
     end
   end
 
-  # DELETE /bookmarks/1
   def destroy
-    # @bookmark = Bookmark.find params[:id]
     @bookmark.destroy
-
-    redirect_to bookmarks_url, notice: 'Bookmark was successfully destroyed.'
+    redirect_to action: :index, notice: 'Bookmark was successfully destroyed.'
   end
 
   private
