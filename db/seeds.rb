3336eb3d4d4f8ca23709bf485954aa76c80d2fe5
shaynaCummings <shayna.cummings@gmail.com>
2014-10-08 17:10:24 -0400

    working nicely but need to rollback migration to rename 'description' to 'comment'

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 Bookmark.create(title: "New York Times", url: "http://nyt.com", description: "", category: :helpful, favorite_flag: false)
 
 Bookmark.create(title: "Gawker", url: "http://gawker.com", description: "indulgence", category: :funny, favorite_flag: true)
+
+Bookmark.create(title: "AOL", url: "http://aol.com", description: "lol", category: :funny, favorite_flag: false)
