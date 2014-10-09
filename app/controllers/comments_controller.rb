e245f929fe9d77c6ee99ab693e7143345a304120
IljaGoushcha <iljagou@yahoo.com>
2014-10-09 16:32:19 -0400

    add comments work now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(allowed_params)
+    @comment.save
     redirect_to :back
   end
+
+  private
+
+  def allowed_params
+    params.require(:comment).permit(:body, :bookmark_id)
+  end
+
 end
