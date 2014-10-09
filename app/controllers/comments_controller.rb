1f9f87373692f3afcf8d6409fd71ad8a42c311af
raielin@gmail.com <raielin@gmail.com>
2014-10-09 00:08:50 -0400

    Created Comment form and defined `new` method in Comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Bookmark.create(comment_params)
+    redirect_to @comment.bookmark
+  end
 
+  private
+  def comment_params
+    params.require(:bookmark).permit(:message, :bookmark_id)
+  end
 
 end
