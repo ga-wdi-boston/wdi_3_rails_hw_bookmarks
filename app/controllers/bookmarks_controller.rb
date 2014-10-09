256a2545ae037daa3605b790acd372f4e49a16c9
IljaGoushcha <iljagou@yahoo.com>
2014-10-09 09:21:17 -0400

    got delete to work

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -14,9 +14,14 @@ class BookmarksController < ApplicationController
     redirect_to action: :index
   end
 
+  def show
+
+  end
+
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to action: :index
+    redirect_to bookmarks_path
   end
 
   def allowed_params
