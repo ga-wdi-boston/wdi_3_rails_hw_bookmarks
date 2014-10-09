da416ebfd550c5d123022802760c513f51f116df
IljaGoushcha <iljagou@yahoo.com>
2014-10-09 11:25:07 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Bookmark.delete_all
+Bookmark.create(url: "https://www.facebook.com", title: "Facebook", description: "social network", category: "Useful", checkbox: "favorite")
+Bookmark.create(url: "http://www.cnn.com", title: "CNN", description: "news", category: "Serious", checkbox: "favorite")
+Bookmark.create(url: "http://www.weather.com", title: "Weather", description: "weather forecast", category: "Useful", checkbox: "not-favorite")
+Bookmark.create(url: "http://www.nhl.com", title: "NHL", description: "NHL hockey", category: "Useful", checkbox: "favorite")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
