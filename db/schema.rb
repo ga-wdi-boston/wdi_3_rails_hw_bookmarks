9adb9782f7197da5d86551ca8ebb90711173938f
shaai <shaai.sattar@gmail.com>
2014-10-08 22:58:42 -0400

    added new migrate file. added code to show new comments in the show.html.erb file. Added more comments for each bookmark in the seed file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008150850) do
+ActiveRecord::Schema.define(version: 20141009022814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,12 @@ ActiveRecord::Schema.define(version: 20141008150850) do
     t.text    "comment"
     t.boolean "favorite"
   end
+
+  create_table "comments", force: true do |t|
+    t.string  "comment"
+    t.integer "bookmark_id"
+  end
+
+  add_index "comments", ["bookmark_id"], name: "index_comments_on_bookmark_id", using: :btree
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
-Bookmark.create(title: "New York Times", url: "http://nytimes.com", favorite: false, comment: "A great news site")
-Bookmark.create(title: "Huffington Post", url: "http://huffingtonpost.com", favorite: false, comment: "Some good news, some clickbait")
-Bookmark.create(title: "General Assembly", url: "http://ga.co", favorite: true, comment: "The best school")
+nytimes = Bookmark.create(title: "New York Times", url: "http://nytimes.com", favorite: false, comment: "A great news site")
+huffington_post = Bookmark.create(title: "Huffington Post", url: "http://huffingtonpost.com", favorite: false, comment: "Some good news, some clickbait")
+general_assembly = Bookmark.create(title: "General Assembly", url: "http://ga.co", favorite: true, comment: "The best school")
+
+nytimes.comments.create(comment: "I love this site!")
+nytimes.comments.create(comment: "A good place to visit if you want to learn.")
+
+huffington_post.create(comment: "Good place to get the lastest news.")
+huffington_post.create(comment: "Where I go to get my daily scoop.")
+
+general_assembly.create(comment: "Join GA's WDI course!")
+general_assembly.create(comment: "This is where I'm studying")
