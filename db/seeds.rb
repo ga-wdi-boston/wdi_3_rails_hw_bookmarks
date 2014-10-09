e9b11ef47a4a9ed31a0b0909694a6a84aac8b8e7
shaai <shaai.sattar@gmail.com>
2014-10-08 23:50:17 -0400

    made changes to seed.rb file to change bookmark variable names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 nytimes = Bookmark.create(title: "New York Times", url: "http://nytimes.com", favorite: false, comment: "A great news site")
-huffington_post = Bookmark.create(title: "Huffington Post", url: "http://huffingtonpost.com", favorite: false, comment: "Some good news, some clickbait")
-general_assembly = Bookmark.create(title: "General Assembly", url: "http://ga.co", favorite: true, comment: "The best school")
+huffington = Bookmark.create(title: "Huffington Post", url: "http://huffingtonpost.com", favorite: false, comment: "Some good news, some clickbait")
+ga = Bookmark.create(title: "General Assembly", url: "http://ga.co", favorite: true, comment: "The best school")
 
 nytimes.comments.create(comment: "I love this site!")
 nytimes.comments.create(comment: "A good place to visit if you want to learn.")
 
-huffington_post.create(comment: "Good place to get the lastest news.")
-huffington_post.create(comment: "Where I go to get my daily scoop.")
+huffington.create(comment: "Good place to get the lastest news.")
+huffington.create(comment: "Where I go to get my daily scoop.")
 
-general_assembly.create(comment: "Join GA's WDI course!")
-general_assembly.create(comment: "This is where I'm studying")
+ga.create(comment: "Join GA's WDI course!")
+ga.create(comment: "This is where I'm studying")
