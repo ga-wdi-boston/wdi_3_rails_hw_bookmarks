cdc22bec64d55c4dff751cddcca12a04af429b5d
Holly Moody <hollymoody@charter.net>
2014-10-09 04:42:52 -0400

    Got edit comment form to display.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   def create
-    binding.pry
     @comment = Comment.create(comment_params)
     redirect_to @comment.bookmark
   end
 
+  def edit
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def update
+    binding.pry
+    @comment.update(comment_params)
+    redirect_to @comment.bookmark
+  end
+
   private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comment_params
     params.require(:comment).permit(:content, :boommark)
   end
